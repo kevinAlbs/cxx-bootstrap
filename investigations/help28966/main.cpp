478f0a604ed7e6a523f36d50dd8760a31540dd14
Kevin Albertson <kevin.albertson@mongodb.com>
2021-11-23 17:29:50 -0500

    add example change stream iteration

--- investigations/help28966/main.cpp
+++ investigations/help28966/main.cpp
@@ -1,7 +1,8 @@
 #include <mongocxx/client.hpp>
+#include <mongocxx/database.hpp>
+#include <mongocxx/collection.hpp>
+#include <mongocxx/change_stream.hpp>
 #include <mongocxx/instance.hpp>
-#include <bsoncxx/builder/basic/kvp.hpp>
-#include <bsoncxx/builder/basic/document.hpp>
 #include <bsoncxx/json.hpp>
 
 #include <cstdlib>
@@ -18,7 +19,19 @@ int main () {
 
     auto client = mongocxx::client(mongocxx::uri(uristr));
     auto db = client.database("db");
-    auto doc = make_document(kvp("ping", 1));
-    auto res = db.run_command (doc.view());
-    std::cout << "ping replied with " << bsoncxx::to_json (res.view());
+    auto cs = db.collection("coll").watch();
+    auto cs_iter = cs.begin(); // sends at most one aggregate command.
+    while (true) {
+        if (cs_iter == cs.end()) {
+            std::cout << "No event yet." << std::endl;
+            cs_iter = cs.begin (); // sends at most one getMore command.
+        } else {
+            bsoncxx::document::view event = *cs_iter;
+            std::cout << "Got event: " << bsoncxx::to_json (event) << std::endl;
+            cs_iter++; // sends at most one getMore command.
+        }
+    }
+    // Note: if a resumable error occurs while iterating a change stream, there
+    // may be repeated attempts of the aggregate command in any of the begin or
+    // iteration calls.
 }
