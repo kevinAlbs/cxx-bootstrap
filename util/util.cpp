b63402ce19d322321a05ead0ad8a0e7ba207d198
Kevin Albertson <kevin.albertson@mongodb.com>
2021-11-23 16:59:01 -0500

    add util

---
+++ util/util.cpp
@@ -0,0 +1,26 @@
+#include <mongocxx/options/apm.hpp>
+#include <bsoncxx/json.hpp>
+#include <iostream>
+
+mongocxx::options::apm get_apm_opts (void) {
+    mongocxx::options::apm opts;
+
+    opts.on_command_started ([](const mongocxx::events::command_started_event& event) {
+        std::cout << "-> command started " << event.command_name() << " " << event.database_name() << " " << bsoncxx::to_json (event.command()) << std::endl;
+    });
+
+    opts.on_command_succeeded ([](const mongocxx::events::command_succeeded_event& event) {
+        std::cout << "<- command succeeded " << event.command_name() << " " << bsoncxx::to_json (event.reply()) << std::endl;
+    });
+
+    opts.on_command_failed ([](const mongocxx::events::command_failed_event& event) {
+        std::cout << "<- command failed " << event.command_name() << " " << bsoncxx::to_json (event.failure()) << std::endl;
+    });
+
+    opts.on_server_changed ([](const mongocxx::events::server_changed_event& event) {
+        std::cout << "server changed " << event.host() << ":" << event.port() << " changed from " << event.previous_description().type() << " to " << event.new_description().type() << std::endl;
+        std::cout << "most recent ismaster " << bsoncxx::to_json(event.new_description().is_master()) << std::endl;
+    });
+    
+    return opts;
+}
---
+++ util/util.hpp
@@ -0,0 +1,5 @@
+#pragma once
+
+#include <mongocxx/options/apm.hpp>
+
+mongocxx::options::apm get_apm_opts (void);
