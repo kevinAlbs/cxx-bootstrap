e9bbc125716f15b6e401224f0023d8e206e9c461
Kevin Albertson <kevin.albertson@mongodb.com>
2021-11-23 17:00:46 -0500

    delete cruft

--- investigations/help20788.sh
+++
@@ -1,3 +0,0 @@
-clang++ --std=c++11 help20788-2.cpp $(PKG_CONFIG_PATH=/Users/kevin.albertson/install/mongo-cxx-driver-help20788/lib/pkgconfig pkg-config --cflags --libs libmongocxx) -o execute_test
-export DYLD_LIBRARY_PATH=/Users/kevin.albertson/install/mongo-cxx-driver-help20788/lib
-./execute_test
--- util.hpp
+++
@@ -1,5 +0,0 @@
-#pragma once
-
-#include <mongocxx/options/apm.hpp>
-
-mongocxx::options::apm get_apm_opts (void);
