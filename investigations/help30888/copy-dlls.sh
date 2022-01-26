35681f0908409d57b1822e49f768c59946f7e1b8
Kevin Albertson <kevin.albertson@10gen.com>
2022-01-26 00:01:41 +0000

    add copy-dlls.sh

---
+++ investigations/help30888/copy-dlls.sh
@@ -0,0 +1,3 @@
+cp /home/Administrator/code/cxx-bootstrap/install/mongo-c-driver-1.20.1/bin/*.dll .
+cp /home/Administrator/code/cxx-bootstrap/install/mongo-cxx-driver-r3.6.6/bin/*.dll .
+cp /home/Administrator/code/cxx-bootstrap/install/libmongocrypt-master/bin/*.dll .
