3bc736e0fa54a4ad0e97801b192c6fd71e2807e8
Kevin Albertson <kevin.albertson@mongodb.com>
2023-12-01 08:18:15 -0500

    add upsertSupplied

---
+++ investigations/upsertSupplied/main.cpp
@@ -0,0 +1,31 @@
+#include <mongocxx/client.hpp>
+#include <mongocxx/instance.hpp>
+#include <bsoncxx/builder/basic/kvp.hpp>
+#include <bsoncxx/builder/basic/document.hpp>
+#include <bsoncxx/json.hpp>
+
+#include <cstdlib>
+#include <iostream>
+
+using namespace bsoncxx::builder::basic;
+int main()
+{
+    auto instance = mongocxx::instance();
+
+    std::string uristr = "mongodb://localhost:27017";
+    if (std::getenv("MONGODB_URI"))
+    {
+        uristr = std::getenv("MONGODB_URI");
+    }
+
+    auto client = mongocxx::client(mongocxx::uri(uristr));
+    auto db = client.database("db");
+    auto cmd = make_document(
+        kvp("update", "coll"),
+        kvp("updates", make_array(make_document(
+                           kvp("q", make_document()), // match all documents.
+                           kvp("u", make_document(kvp("foo", "bar"))),
+                           kvp("upsertSupplied", false)))));
+    auto res = db.run_command(cmd.view());
+    std::cout << "command replied with " << bsoncxx::to_json(res.view()) << std::endl;
+}
