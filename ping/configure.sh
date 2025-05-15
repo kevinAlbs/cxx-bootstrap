4a8a10939709d681be2d07dfd15aa5e795c8d45a
Kevin Albertson <kevin.albertson@mongodb.com>
2025-05-15 14:17:28 -0400

    revise

--- ping/configure.sh
+++ ping/configure.sh
@@ -1,6 +1,7 @@
 cmake \
-    -DCMAKE_PREFIX_PATH=../../install/mongo-cxx-driver-r4.0.0 \
+    -DCMAKE_PREFIX_PATH=../install/mongo-cxx-driver-r4.0.0 \
     -DCMAKE_BUILD_TYPE="Debug" \
     -DCMAKE_CXX_STANDARD=17 \
+    -DCMAKE_EXPORT_COMPILE_COMMANDS=ON \
     -S./ \
     -B./cmake-build
