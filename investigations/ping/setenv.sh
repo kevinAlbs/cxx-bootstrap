fccbebe21cb91f565f18dd92381ca880921409a1
Kevin Albertson <kevin.albertson@mongodb.com>
2025-05-15 14:13:58 -0400

    relocate ping

--- investigations/ping/setenv.sh
+++
@@ -1,2 +0,0 @@
-export DYLD_LIBRARY_PATH=/Users/kevin.albertson/install/mongo-c-driver-1.23.2/lib
-export PKG_CONFIG_PATH=/Users/kevin.albertson/install/mongo-c-driver-1.23.2/lib/pkgconfig
--- ping/configure.sh
+++ ping/configure.sh
@@ -1,5 +1,5 @@
 cmake \
-    -DCMAKE_PREFIX_PATH=../../install/mongo-cxx-driver-r3.7.0 \
+    -DCMAKE_PREFIX_PATH=../../install/mongo-cxx-driver-r4.0.0 \
     -DCMAKE_BUILD_TYPE="Debug" \
     -DCMAKE_CXX_STANDARD=17 \
     -S./ \
