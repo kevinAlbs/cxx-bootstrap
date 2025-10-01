07e7044c20bfaad4a490d3fdd3a6b8740c3a0cbc
Kevin Albertson <kevin.albertson@mongodb.com>
2025-10-01 08:11:16 -0400

    update again

--- investigations/apm_example/main.cpp
+++ investigations/apm_example/main.cpp
@@ -1,3 +1,23 @@
+/*
+Show an example of printing a server changed event. Example:
+
+ping ... begin
+server changed localhost:27017
+    old: Unknown { }
+    new: RSPrimary { "helloOk" : true, "topologyVersion" : { "processId" : { "$oid" : "68da75a0b983a74e5a236920" },
+"counter" : 61 }, "hosts" : [ "localhost:27017" ], "passives" : [ "localhost:27018" ], "setName" : "oidc-repl0",
+"setVersion" : 1, "ismaster" : true, "secondary" : false, "primary" : "localhost:27017", "me" : "localhost:27017",
+"electionId" : { "$oid" : "7fffffff000000000000000d" }, "lastWrite" : { "opTime" : { "ts" : { "$timestamp" : { "t" :
+1759320544, "i" : 1 } }, "t" : 13 }, "lastWriteDate" : { "$date" : "2025-10-01T12:09:04Z" }, "majorityOpTime" : { "ts" :
+{ "$timestamp" : { "t" : 1759320544, "i" : 1 } }, "t" : 13 }, "majorityWriteDate" : { "$date" : "2025-10-01T12:09:04Z" }
+}, "maxBsonObjectSize" : 16777216, "maxMessageSizeBytes" : 48000000, "maxWriteBatchSize" : 100000, "localTime" : {
+"$date" : "2025-10-01T12:09:04.652Z" }, "logicalSessionTimeoutMinutes" : 30, "connectionId" : 760, "minWireVersion" : 0,
+"maxWireVersion" : 28, "readOnly" : false, "ok" : 1.0, "$clusterTime" : { "clusterTime" : { "$timestamp" : { "t" :
+1759320544, "i" : 1 } }, "signature" : { "hash" : { "$binary" : { "base64" : "lbev6+1+fmJXvHadj/E2CCBwf48=", "subType" :
+"00" } }, "keyId" : 7555480633447809029 } }, "operationTime" : { "$timestamp" : { "t" : 1759320544, "i" : 1 } } } ping
+... end
+
+*/
 #include <bsoncxx/builder/basic/document.hpp>
 #include <bsoncxx/builder/basic/kvp.hpp>
 #include <bsoncxx/json.hpp>
@@ -35,6 +55,7 @@ int main() {
   auto client = mongocxx::client(mongocxx::uri(uristr), client_opts);
   auto db = client.database("db");
   auto doc = make_document(kvp("ping", 1));
+  std::cout << "ping ... begin" << std::endl;
   auto res = db.run_command(doc.view());
-  std::cout << "ping replied with " << bsoncxx::to_json(res.view()) << std::endl;
+  std::cout << "ping ... end" << std::endl;
 }
